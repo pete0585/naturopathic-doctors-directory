4631228c2bfac684b62d0378ba9e0c9b214b2ae4
Adam Peters <adam@thestrategicveteran.com>
2026-09-24 10:54:54 -0400

    fix: require verified listing ownership for contact changes

---
+++ lib/claim-config.ts
@@ -0,0 +1,9 @@
+export const CLAIM_CONFIG = {
+  "listings": "nd_listings",
+  "claims": "nd_claims",
+  "claimedFields": [
+    "claimed_at"
+  ],
+  "siteUrl": "https://www.naturopathicdoctorfinder.com",
+  "from": "hello@mail.naturopathicdoctorfinder.com"
+} as const
